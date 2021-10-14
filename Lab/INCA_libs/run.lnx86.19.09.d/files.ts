eecd12d72cb7531f83444a03904765d934c61d54
Agnieszka Kamien <akamien@student.agh.edu.pl>
2021-10-14 14:52:27 +0200

    Lab folder created

---
+++ Lab/INCA_libs/run.lnx86.19.09.d/files.ts
@@ -0,0 +1,3 @@
+1552898862 /home/student/akamien/cds.lib
+1633612569 /home/student/akamien/VDIC/Lab0/apple.sv
+1633815425 /home/student/akamien/VDIC/Lab0/apple_tb.sv
---
+++ Lab/INCA_libs_gui/run.lnx86.19.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1633612569 /home/student/akamien/VDIC/Lab0/apple.sv
+1633815282 /home/student/akamien/VDIC/Lab0/apple_tb.sv
